9720470501c523ee14e7b6db0db07228a81e9c52
Samuel Mullen <samullen@gmail.com>
2012-08-31 14:12:08 -0500

    add omniauth callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ossguidepost::Application.routes.draw do
+  devise_for :user, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
