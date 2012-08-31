e6cd1c322aedd67ac4b5ead7a7f692dc75f24ca4
Samuel Mullen <samullen@gmail.com>
2012-08-31 09:58:08 -0500

    initial offering

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ossguidepost::Application.config.secret_token = '0a96ff48a2471eead6ae7cdc5eb8b7bc777135d8cfbfe3b1ba20e841b43077f2ca19bdde5307758586f6b828866e4eeb7993a91a3cdef96868da6acb0de6bb59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ossguidepost::Application.config.session_store :cookie_store, key: '_ossguidepost_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ossguidepost::Application.config.session_store :active_record_store
