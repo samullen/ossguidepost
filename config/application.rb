bcc5a664d2be52d0402d05da3783155e03af05ae
Samuel Mullen <samullen@gmail.com>
2012-08-31 13:30:53 -0500

    nix view and controller specs

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Ossguidepost
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -68,7 +68,8 @@ module Ossguidepost
     config.assets.initialize_on_precompile = false
 
     config.generators do |g|
-      g.test_framework :mini_test, :spec => true, :fixture => false
+      g.view_specs false
+      g.controller_specs false
     end
   end
 end
