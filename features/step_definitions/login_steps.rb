b8543d9efb8320e65521d255b2d5ee792956544b
Samuel Mullen <samullen@gmail.com>
2012-09-03 16:13:10 -0500

    add project import feature

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -6,6 +6,12 @@ Given /^I have an account$/ do
   authentication = FactoryGirl.create :authentication, :user => @current_user
 end
 
+Given /^I am signed in through GitHub$/ do
+  step "I have an account"
+  visit root_path
+  step "I sign in with GitHub"
+end
+
 When /^I sign (?:up|in) (?:with|through) GitHub$/ do
   find(:link, "github-authentication").click
 end
