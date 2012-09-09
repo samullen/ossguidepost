fcd38b3ea2a0e6719d932961e88f8b11f4022b14
Samuel Mullen <samullen@gmail.com>
2012-09-08 22:20:12 -0500

    finish github import method

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,4 +1,7 @@
 class Import
   def self.github_import(username)
+    raise ArgumentError unless block_given?
+
+    Octokit.repos(username).map { |repo| yield(repo) unless repo.private }.compact
   end
 end
--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -4,13 +4,21 @@ describe Import do
   describe "github_import" do
     it "requires a name from which to pull repos" do
       lambda { Import.github_import }.should raise_error(ArgumentError)
-      lambda { Import.github_import("username") }.should_not raise_error(ArgumentError)
+      lambda { Import.github_import("username") {} }.should_not raise_error(ArgumentError)
     end
 
-    it "returns an array of Projects"
+    it "requires a block" do
+      lambda { Import.github_import("username") }.should raise_error(ArgumentError)
+    end
 
-    it "retrieves repos for the specified user only"
+    it "returns an array of objects specified in the block" do
+      projects = Import.github_import("username") {|repo| repo}
+      projects.should_not be_empty
+    end
 
-    it "skips private repos"
+    it "skips private repos" do
+      projects = Import.github_import("username") {|repo| repo}
+      projects.any? {|project| project.name == "wordsearch" }.should be_false
+    end
   end
 end
