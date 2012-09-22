4f19e0bccca869c6fcd33b734373af960850705e
Samuel Mullen <samullen@gmail.com>
2012-09-21 23:10:38 -0500

    add edit and update methods. edit.html.erb view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,17 @@ class ProjectsController < ApplicationController
     user = User.find(params[:user_id])
     @project = Project.find_by_name(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @project = Project.find_by_name(params[:id])
+  end
+
+  def update
+    @project = current_user.projects.find_by_name(params[:id])
+
+    if @project.update_attributes(params[:project])
+      redirect_to(user_project_path(current_user, @project), :notice => 'Project was successfully updated.')
+    end
+  end
 end
